15f3b71445f7dc798cea6112761a92a4884a24e1
Shubham <71990977+Skparab1@users.noreply.github.com>
2021-06-02 18:34:06 -0700

    Add files via upload

--- pong.py
+++ pong.py
@@ -7,15 +7,17 @@ print('Pong')
 print('How to play: press contol + c to move your paddle up')
 name = input('enter your name ')
 if name == 'autoplay':
-    autoplay = True
+    autoplay, name = True, 'Autoplay Computer'
 pos = 49
 angle = 2
 score = 0
 waittime = 0.1
+def addmidline(line):
+    return line[:49] + '|' + line[50:]
 def printarena(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,line14,line15,line16,line17,line18):
-    global p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18
-    print(' '+('_'*100))
-    print(s18+line18+p18),print(s17+line17+p17),print(s16+line16+p16),print(s15+line15+p15),print(s14+line14+p14),print(s13+line13+p13),print(s12+line12+p12),print(s11+line11+p11),print(s10+line10+p10),print(s9+line9+p9),print(s8+line8+p8),print(s7+line7+p7),print(s6+line6+p6),print(s5+line5+p5),print(s4+line4+p4),print(s3+line3+p3),print(s2+line2+p2),print(s1+line1+p1)
+    global p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18, name
+    print('\n'*50),print('Pypong Pro (computer)'+(' '*70)+name),print(' '+('_'*100))
+    print(s18+addmidline(line18)+p18),print(s17+addmidline(line17)+p17),print(s16+addmidline(line16)+p16),print(s15+addmidline(line15)+p15),print(s14+addmidline(line14)+p14),print(s13+addmidline(line13)+p13),print(s12+addmidline(line12)+p12),print(s11+addmidline(line11)+p11),print(s10+addmidline(line10)+p10),print(s9+addmidline(line9)+p9),print(s8+addmidline(line8)+p8),print(s7+addmidline(line7)+p7),print(s6+addmidline(line6)+p6),print(s5+addmidline(line5)+p5),print(s4+addmidline(line4)+p4),print(s3+addmidline(line3)+p3),print(s2+addmidline(line2)+p2),print(s1+addmidline(line1)+p1)
     print(' '+('-'*100))
 def removeball(line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,line14,line15,line16,line17,line18):
     line1,line2,line3,line4,line5,line6,line7,line8,line9,line10,line11,line12,line13,line14,line15,line16,line17,line18 = line1.replace('o',' '),line2.replace('o',' '),line3.replace('o',' '),line4.replace('o',' '),line5.replace('o',' '),line6.replace('o',' '),line7.replace('o',' '),line8.replace('o',' '),line9.replace('o',' '),line10.replace('o',' '),line11.replace('o',' '),line12.replace('o',' '),line13.replace('o',' '),line14.replace('o',' '),line15.replace('o',' '),line16.replace('o',' '),line17.replace('o',' '),line18.replace('o',' ')
@@ -114,13 +116,13 @@ while True:
         if ((pos <= 40 and angle >= 3) or (pos <= 25 and angle == 2) or pos <= 20) and (movedirection == 'left' or movedirection == 'leftup' or movedirection == 'leftdown'):
             if spos > linein:
                 spos -= 1
-            if spos <= linein:
+            if spos <= linein and spos <= 13:
                 spos += 1   
             s1,s2,s3,s4,s5,s6,s7,s8,s9,s10,s11,s12,s13,s14,s15,s16,s17,s18 = '|' if spos == 1 else ' ','|' if spos == 1 or spos == 2 else ' ','|' if spos == 1 or spos == 2 or spos == 3 else ' ','|' if spos == 1 or spos == 2 or spos == 3 or spos == 4 else ' ','|' if spos == 2 or spos == 3 or spos == 4 or spos == 5 else ' ','|' if spos == 3 or spos == 4 or spos == 5 or spos == 6 else ' ','|' if spos == 4 or spos == 5 or spos == 6 or spos == 7 else ' ','|' if spos == 5 or spos == 6 or spos == 7 or spos == 8 else ' ','|' if spos == 6 or spos == 7 or spos == 8 or spos == 9 else ' ','|' if spos == 7 or spos == 8 or spos == 9 or spos == 10 else ' ','|' if spos == 8 or spos == 9 or spos == 10 or spos == 11 else ' ','|' if spos == 9 or spos == 10 or spos == 11 or spos == 12 else ' ','|' if spos == 10 or spos == 11 or spos == 12 or spos == 12 else ' ','|' if spos == 11 or spos == 12 or spos == 13 or spos == 14 else ' ','|' if spos == 12 or spos == 13 or spos == 14 or spos == 15 else ' ','|' if spos == 13 or spos == 14 or spos == 15 or spos == 16 else ' ','|' if spos == 14 or spos == 15 or spos == 16 or spos == 17 else ' ','|' if spos == 15 or spos == 16 or spos == 17 or spos == 18 else ' '
         if (((pos >= 60 and angle >= 3) or (pos >= 75 and angle == 2) or pos >= 85) and (movedirection == 'right' or movedirection == 'rightup' or movedirection == 'rightdown')) and autoplay == True:
             if apos > linein:
                 apos -= 1
-            if apos <= linein:
+            if apos <= linein and apos <= 13:
                 apos += 1
             padpos = apos
         p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18 = '|' if padpos == 1  or padpos == 2 else '','|' if padpos == 1 or padpos == 2 or padpos == 3 else '','|' if padpos == 1 or padpos == 2 or padpos == 3  or padpos == 4 else '','|' if padpos == 1 or padpos == 2 or padpos == 3 or padpos == 4 or padpos == 5 else '','|' if padpos == 2 or padpos == 3 or padpos == 4 or padpos == 5 or padpos == 6 else '','|' if padpos == 3 or padpos == 4 or padpos == 5 or padpos == 6  or padpos == 7 else '','|' if padpos == 4 or padpos == 5 or padpos == 6 or padpos == 7  or padpos == 8 else '','|' if padpos == 5 or padpos == 6 or padpos == 7 or padpos == 8  or padpos == 9 else '','|' if padpos == 6 or padpos == 7 or padpos == 8 or padpos == 9  or padpos == 10 else '','|' if padpos == 7 or padpos == 8 or padpos == 9 or padpos == 10  or padpos == 11 else '','|' if padpos == 8 or padpos == 9 or padpos == 10 or padpos == 11 or padpos == 12 else '','|' if padpos == 9 or padpos == 10 or padpos == 11 or padpos == 12  or padpos == 13 else '','|' if padpos == 10 or padpos == 11 or padpos == 12 or padpos == 13 or padpos == 14 else '','|' if padpos == 11 or padpos == 12 or padpos == 13 or padpos == 14 or padpos == 15 else '','|' if padpos == 12 or padpos == 13 or padpos == 14 or padpos == 15  or padpos == 16 else '','|' if padpos == 13 or padpos == 14 or padpos == 15 or padpos == 16  or padpos == 17 else '','|' if padpos == 14 or padpos == 15 or padpos == 16 or padpos == 17  or padpos == 18 else '','|' if padpos == 14 or padpos == 15 or padpos == 16 or padpos == 17 or padpos == 18 else ''
@@ -129,6 +131,6 @@ while True:
             padpos = padstorer
             padpos -= 0.3
     except:
-        if padpos <= 17 and autoplay == False:
+        if padpos <= 13 and autoplay == False:
             padpos += 2
 print('your score was ',score)
